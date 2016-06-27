a19fd8b92cbb5a7616e016408faee41fde38b728
tarurar <tarutin_andrew@mail.ru>
2016-06-27 22:46:44 +0400

    test console project added

---
+++ GisSync.TestConsole/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+using GisSync.Model;
+using GisSync.Engine.Workers;
+using GisSync.Engine.SyncDefinitions;
+using GisSync.Workers;
+
+namespace GisSync.TestConsole
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			var model = new SyncSqlModel
+			{
+				Nodes = new Queue<BaseSqlDefinition>(new BaseSqlDefinition[] 
+				{
+					new BaseSqlDefinition 
+					{
+						Workers = new Queue<ISqlWorker>(new ISqlWorker[] 
+						{
+							new Workers.Parcels.CreateWorker(),
+				           	new Workers.Parcels.UpdateWorker(),
+				           	new Workers.Parcels.DeleteWorker()           
+						})
+					}
+				})
+			};
+
+			var n1 = model.Nodes.Dequeue();
+			var w1 = n1.Workers.Dequeue();
+			w1.Execute(null, null, (percent) => 
+			{ 
+				Console.WriteLine("Percentage: {0}", percent);
+			});
+		}
+	}
+}
